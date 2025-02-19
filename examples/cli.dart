5b1cf77ba2d8726cdbff0ba6d6bd9f41ce6e0400
kieran <kieran@harkin.me>
2025-02-19 16:40:21 +0000

    print manifest info in example cli tool

--- examples/cli.dart
+++ examples/cli.dart
@@ -4,5 +4,7 @@ import 'package:apk_parser/src/rust/frb_generated.dart';
 Future<void> main(List<String> args) async {
   await RustLib.init();
   final parser = ApkParser(path: args[0]);
-  parser.loadManifest();
+  final manifest = parser.loadManifest();
+
+  print("id=${manifest.package}\nmin_sdk_version=${manifest.minSdkVersion}");
 }
