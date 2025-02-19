3a54f732767b919872aa858e6d26876f8a001ebc
kieran <kieran@harkin.me>
2025-02-19 16:32:48 +0000

    add cli example

---
+++ examples/cli.dart
@@ -0,0 +1,8 @@
+import 'package:apk_parser/src/rust/api.dart';
+import 'package:apk_parser/src/rust/frb_generated.dart';
+
+Future<void> main(List<String> args) async {
+  await RustLib.init();
+  final parser = ApkParser(path: args[0]);
+  parser.loadManifest();
+}
